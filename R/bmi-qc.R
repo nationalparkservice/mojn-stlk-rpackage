357d9e6c8d0ffc323207fd6d0983560911db0d0f
Sarah E. Wright <s.wright.618@gmail.com>
2021-09-14 11:32:04 -0700

    Fix BMILong

--- R/bmi-qc.R
+++ R/bmi-qc.R
@@ -74,7 +74,7 @@ BMILong <- function(conn, path.to.data, park, site, field.season, data.source = 
     dplyr::mutate(TaxaGroup = gsub("Abundance", "", TaxaGroup),
                   TaxaGroup = gsub("Taxa", "", TaxaGroup))
 
-  bmi_long <- dplyr::inner_join(count_pivot, abundance_pivot, by = c("Park", "SiteShort", "SiteCode", "SiteName", "FieldSeason", "VisitDate", "VisitType", "SampleType", "SampleCollectionMethod", "DPL", "BMIMethod", "LabSampleNumber", "DateCollected", "LabNotes", "FieldNotes", "SampleArea_m2", "FieldSplit", "LabSplit", "SplitCount", "FixedCount", "BigRareCount", "ShannonsDiversity", "SimpsonsDiversity", "Hilsenhoff", "Evenness", "USFSCommunityToleranceQuo", "DominantFamily", "DominantFamilyPercent", "DominantTaxon", "DominantTaxonPercent", "LabName", "ID", "TaxaGroup"))
+  bmi_long <- dplyr::inner_join(count_pivot, abundance_pivot, by = c("Park", "SiteShort", "SiteCode", "SiteName", "FieldSeason", "VisitDate", "VisitType", "SampleType", "SampleCollectionMethod", "DPL", "BMIMethod", "LabSampleNumber", "DateCollected", "LabNotes", "FieldNotes", "SampleArea_m2", "FieldSplit", "LabSplit", "SplitCount", "FixedCount", "BigRareCount", "ShannonsDiversity", "SimpsonsDiversity", "Hilsenhoff", "Evenness", "USFSCommunityToleranceQuo", "DominantFamily", "DominantFamilyPercent", "DominantTaxon", "DominantTaxonPercent", "LabName", "TaxaGroup"))
 
   # Throw error if join gets messed up somehow
   if ((nrow(bmi_long) != nrow(count_pivot)) | (nrow(bmi_long) != nrow(abundance_pivot))) {
